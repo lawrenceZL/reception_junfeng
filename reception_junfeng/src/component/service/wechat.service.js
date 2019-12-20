cd899a81d1a44782659a85eef0b4b551c157986d
lawrenceZL <40664153+lawrenceZL@users.noreply.github.com>
2019-12-20 15:28:42 +0800

    bug fix

--- reception_junfeng/src/component/service/wechat.service.js
+++ reception_junfeng/src/component/service/wechat.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const wechat_url = 'https://www.jfczn.com/wechat';
+const wechat_url = 'https://www.jfczn.com/reception/wechat';
 
 function configuration(url) {
     let request_config_url = wechat_url + '/config/init';
