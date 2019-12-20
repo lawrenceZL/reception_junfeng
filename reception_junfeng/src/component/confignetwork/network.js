cb5b97ce480cf3243d96086d4ef012599a843af1
lawrenceZL <40664153+lawrenceZL@users.noreply.github.com>
2019-12-20 17:03:14 +0800

    app id

--- reception_junfeng/src/component/confignetwork/network.js
+++ reception_junfeng/src/component/confignetwork/network.js
@@ -105,7 +105,7 @@ class NetworkConfig extends React.Component {
                     window.wx.config({
                         beta: true,
                         debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                        appId: result.appId, // 必填，公众号的唯一标识
+                        appId: 'wxf20d52663db7b9bc', // 必填，公众号的唯一标识
                         timestamp: result.timestamp, // 必填，生成签名的时间戳
                         nonceStr: result.nonceStr, // 必填，生成签名的随机串
                         signature: result.signature,// 必填，签名
